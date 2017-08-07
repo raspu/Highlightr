a2df9db1b1798050d66a4489edc796f54e314dbe
Illanes, Juan Pablo <jpillanes@comscore.com>
2017-08-07 23:54:59 +0200

    Added Carthage project (WIP)

---
+++ Highlightr/Highlightr.h
@@ -0,0 +1,19 @@
+//
+//  Highlightr.h
+//  Highlightr
+//
+//  Created by Illanes, Juan Pablo on 8/7/17.
+//  Copyright © 2017 Illanes, Juan Pablo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Highlightr.
+FOUNDATION_EXPORT double HighlightrVersionNumber;
+
+//! Project version string for Highlightr.
+FOUNDATION_EXPORT const unsigned char HighlightrVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Highlightr/PublicHeader.h>
+
+
